1f097dac92d25079123e02c9653cfa95820f9ff8
nakashraza6565-cell <nakashraza6565@gmail.com>
2026-04-26 04:53:14 -0700

    Final: Removed red bar, moved assets to root, and cleaned up debug files

--- drautos/resources/views/frontend/layouts/master.blade.php
+++ drautos/resources/views/frontend/layouts/master.blade.php
@@ -4,9 +4,7 @@
 	@include('frontend.layouts.head')	
 </head>
 <body class="js">
-	<div style="background: red; color: white; padding: 20px; text-align: center; font-weight: bold; font-size: 24px; z-index: 9999; position: relative;">
-		GLOBAL DEPLOYMENT TEST: IF YOU SEE THIS, UPDATE IS WORKING!
-	</div>
+	
 	
 	<!-- Preloader -->
 	<div class="preloader">
