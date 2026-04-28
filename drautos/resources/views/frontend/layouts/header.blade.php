73df93661207c23bb54a68ba4f1870338c5aa373
nakashraza6565-cell <nakashraza6565@gmail.com>
2026-04-28 06:39:28 -0700

    UI: Pointed header login link to standard /login route

--- drautos/resources/views/frontend/layouts/header.blade.php
+++ drautos/resources/views/frontend/layouts/header.blade.php
@@ -25,7 +25,7 @@
                                 @endif
                                 <li><i class="ti-power-off"></i> <a href="{{route('user.logout')}}">Logout</a></li>
                             @else
-                                <li><i class="ti-power-off"></i><a href="{{route('login.form')}}">Login /</a> <a href="{{route('register.form')}}">Register</a></li>
+                                <li><i class="ti-power-off"></i><a href="{{route('login')}}">Login /</a> <a href="{{route('register.form')}}">Register</a></li>
                             @endauth
                         </ul>
                     </div>
