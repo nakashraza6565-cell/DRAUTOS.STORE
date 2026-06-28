308a3764d9ceca2e39b5f7cc3373b5fc1aa87a40
nakashraza6565-cell <nakashraza6565@gmail.com>
2026-06-28 05:06:44 -0700

    Auto-Update: 2026-06-28 05:06:44

--- drautos/resources/views/backend/index.blade.php
+++ drautos/resources/views/backend/index.blade.php
@@ -344,7 +344,6 @@
         </div>
     </div>
 
-</div>
 <!-- Quick Attendance Modal -->
   <div class="modal fade" id="quickAttendanceModal" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
